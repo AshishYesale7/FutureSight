b338a77b5de26128f9a244674260561f39342d7a
Ashish Yesale <ashishyesale007@gmail.com>
2025-06-09 05:15:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,7 @@ export default function RootLayout({
 }>) {
   // The useTheme hook will handle adding/removing 'dark' class on client side
   return (
-    <html lang="en" suppressHydrationWarning>{/* suppressHydrationWarning for class on html */}
-      <head>
+    <html lang="en" suppressHydrationWarning>{/* suppressHydrationWarning for class on html */}<head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
