7fd6ff1ee300148cf73c6c8eba2829d118254151
Ashish Yesale <ashishyesale007@gmail.com>
2025-06-20 22:12:37 +0000

    the popup of add new Event is too big its not screen fit neither scrolla

--- src/components/timeline/EditEventModal.tsx
+++ src/components/timeline/EditEventModal.tsx
@@ -49,17 +49,18 @@ const EditEventModal: FC<EditEventModalProps> = ({
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-lg frosted-glass p-0">
-        <DialogHeader className="p-6 pb-4">
+        <DialogHeader className="p-6 pb-4 border-b border-border/30">
           <DialogTitle className="font-headline text-xl text-primary">{dialogTitle}</DialogTitle>
           <DialogDescription>
             {dialogDescription}
           </DialogDescription>
         </DialogHeader>
-        <div className="px-6 pb-6 pt-2">
+        <div className="px-6 pb-6 pt-4 overflow-y-auto max-h-[calc(100vh-15rem)] sm:max-h-[70vh]">
           <EditEventForm
             eventToEdit={eventToEdit}
             onSubmit={handleFormSubmit}
             onCancel={handleCancel}
+            isAddingNewEvent={isAddingNewEvent}
           />
         </div>
       </DialogContent>
@@ -68,3 +69,4 @@ const EditEventModal: FC<EditEventModalProps> = ({
 };
 
 export default EditEventModal;
+
